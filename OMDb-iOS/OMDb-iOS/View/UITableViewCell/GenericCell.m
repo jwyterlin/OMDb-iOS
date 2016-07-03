7be91036df0b0aafff7db5dfc4e0f7c09a3c9b5b
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2016-07-03 15:12:22 -0300

    Created class GenericCell

---
+++ OMDb-iOS/OMDb-iOS/View/UITableViewCell/GenericCell.m
@@ -0,0 +1,13 @@
+//
+//  GenericCell.m
+//  OMDb-iOS
+//
+//  Created by Jhonathan Wyterlin on 03/07/16.
+//  Copyright © 2016 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericCell.h"
+
+@implementation GenericCell
+
+@end
