621ff2d55204e7a99a19e3d20e0d55a655626cbf
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2016-07-14 01:01:00 -0300

    Created more unit tests for Constant class

--- OMDb-iOS/OMDb-iOSTests/ConstantTests.m
+++ OMDb-iOS/OMDb-iOSTests/ConstantTests.m
@@ -33,10 +33,154 @@
     
 }
 
+#pragma mark - Nib Name - Cell
+
 -(void)testNibNameMovieCell {
-    
     XCTAssert( [@"MovieCell" isEqualToString:kNibNameMovieCell] );
-    
+}
+
+#pragma mark - Model - Keys
+
+#pragma mark - Movie
+
+-(void)testMovieActors {
+    XCTAssert( [@"Actors" isEqualToString:kMovieActors] );
+}
+
+-(void)testMovieAwards {
+    XCTAssert( [@"Awards" isEqualToString:kMovieAwards] );
+}
+
+-(void)testMovieCountry {
+    XCTAssert( [@"Country" isEqualToString:kMovieCountry] );
+}
+
+-(void)testMovieDirector {
+    XCTAssert( [@"Director" isEqualToString:kMovieDirector] );
+}
+
+-(void)testMovieGenre {
+    XCTAssert( [@"Genre" isEqualToString:kMovieGenre] );
+}
+
+-(void)testMovieLanguage {
+    XCTAssert( [@"Language" isEqualToString:kMovieLanguage] );
+}
+
+-(void)testMovieMetascore {
+    XCTAssert( [@"Metascore" isEqualToString:kMovieMetascore] );
+}
+
+-(void)testMoviePlot {
+    XCTAssert( [@"Plot" isEqualToString:kMoviePlot] );
+}
+
+-(void)testMoviePoster {
+    XCTAssert( [@"Poster" isEqualToString:kMoviePoster] );
+}
+
+-(void)testMovieRated {
+    XCTAssert( [@"Rated" isEqualToString:kMovieRated] );
+}
+
+-(void)testMovieReleased {
+    XCTAssert( [@"Released" isEqualToString:kMovieReleased] );
+}
+
+-(void)testMovieResponse {
+    XCTAssert( [@"Response" isEqualToString:kMovieResponse] );
+}
+
+-(void)testMovieRuntime {
+    XCTAssert( [@"Runtime" isEqualToString:kMovieRuntime] );
+}
+
+-(void)testMovieTitle {
+    XCTAssert( [@"Title" isEqualToString:kMovieTitle] );
+}
+
+-(void)testMovieType {
+    XCTAssert( [@"Type" isEqualToString:kMovieType] );
+}
+
+-(void)testMovieWriter {
+    XCTAssert( [@"Writer" isEqualToString:kMovieWriter] );
+}
+
+-(void)testMovieYear {
+    XCTAssert( [@"Year" isEqualToString:kMovieYear] );
+}
+
+-(void)testMovieImdbID {
+    XCTAssert( [@"imdbID" isEqualToString:kMovieImdbID] );
+}
+
+-(void)testMovieImdbRating {
+    XCTAssert( [@"imdbRating" isEqualToString:kMovieImdbRating] );
+}
+
+-(void)testMovieImdbVotes {
+    XCTAssert( [@"imdbVotes" isEqualToString:kMovieImdbVotes] );
+}
+
+#pragma mark - OMDB Parameter Keys
+
+-(void)testOMDBParameterGetByID {
+    XCTAssert( [@"i" isEqualToString:kOMDBParameterGetByID] );
+}
+
+-(void)testOMDBParameterGetByTitle {
+    XCTAssert( [@"t" isEqualToString:kOMDBParameterGetByTitle] );
+}
+
+-(void)testOMDBParameterType {
+    XCTAssert( [@"type" isEqualToString:kOMDBParameterType] );
+}
+
+-(void)testOMDBParameterYearRelease {
+    XCTAssert( [@"y" isEqualToString:kOMDBParameterYearRelease] );
+}
+
+-(void)testOMDBParameterPlot {
+    XCTAssert( [@"plot" isEqualToString:kOMDBParameterPlot] );
+}
+
+-(void)testOMDBParameterDataTypeReturn {
+    XCTAssert( [@"r" isEqualToString:kOMDBParameterDataTypeReturn] );
+}
+
+-(void)testOMDBParameterIncludeTomatoesRating {
+    XCTAssert( [@"tomatoes" isEqualToString:kOMDBParameterIncludeTomatoesRating] );
+}
+
+-(void)testOMDBParameterJSONPCallbackName {
+    XCTAssert( [@"callback" isEqualToString:kOMDBParameterJSONPCallbackName] );
+}
+
+-(void)testOMDBParameterAPIVersion {
+    XCTAssert( [@"v" isEqualToString:kOMDBParameterAPIVersion] );
+}
+
+-(void)testOMDBParameterSearchByTerm {
+    XCTAssert( [@"s" isEqualToString:kOMDBParameterSearchByTerm] );
+}
+
+-(void)testOMDBParameterSearchPage {
+    XCTAssert( [@"page" isEqualToString:kOMDBParameterSearchPage] );
+}
+
+#pragma mark - Result Keys
+
+-(void)testSearch {
+    XCTAssert( [@"Search" isEqualToString:kSearch] );
+}
+
+-(void)testResponse {
+    XCTAssert( [@"Response" isEqualToString:kResponse] );
+}
+
+-(void)testError {
+    XCTAssert( [@"Error" isEqualToString:kError] );
 }
 
 @end
