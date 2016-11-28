e9fa9dc2c353efdae302985240e70f214a432b9b
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2016-11-28 21:55:51 -0200

    Created test method to check if is possible instantiate AddMovieConductor

--- OMDb-iOS/OMDb-iOSTests/AddMovieConductorTests.m
+++ OMDb-iOS/OMDb-iOSTests/AddMovieConductorTests.m
@@ -8,32 +8,37 @@
 
 #import <XCTest/XCTest.h>
 
+// Conductor
+#import "AddMovieConductor.h"
+
 @interface AddMovieConductorTests : XCTestCase
 
 @end
 
 @implementation AddMovieConductorTests
 
-- (void)setUp {
+-(void)setUp {
+    
     [super setUp];
+    
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
 }
 
-- (void)tearDown {
+-(void)tearDown {
+ 
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
     [super tearDown];
+    
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+-(void)testInstantiate {
+    
+    AddMovieConductor *addMovieConductor = [AddMovieConductor new];
+    
+    XCTAssert( addMovieConductor );
+    
 }
 
 @end
